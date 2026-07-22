37bba362b9968fb02432e2af5fe97c0466c234dc
Alan Gonzalez <alanagustin724@gmail.com>
2026-07-22 17:52:00 -0300

    datos iniciales

---
+++ src/main/java/com/gestor/ProductivityInsights/ProductivityInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.gestor.ProductivityInsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivityInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductivityInsightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestor/ProductivityInsights/ProductivityInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestor.ProductivityInsights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductivityInsightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
